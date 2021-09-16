683e0f48fb842b5638d896aed03f2aa3cf947c81
git-avinash <mail.avinashsah@gmail.com>
2021-09-16 11:35:43 +0530

    fix

--- helpers.py
+++ helpers.py
@@ -20,7 +20,8 @@ def send_message_to_discord(webhook, message_by, message):
 
 def handle_wadc_bridge(form):
     payload_title = form.get("title")
-    parsed_title = [x.strip() for x in payload_title.split(":") if x.strip() != ""]
+    parsed_title = [x.strip()
+                    for x in payload_title.split(":") if x.strip() != ""]
     payload_text = f"```{form.get('text')}```"
     sender = parsed_title[-1] if len(parsed_title) >= 2 else "Unknown"
 
@@ -32,11 +33,12 @@ def handle_wadc_bridge(form):
         send_message_to_discord(
             OFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
 
-    elif "BSC CS" in payload_title:
-        send_message_to_discord(UNOFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
-
     elif "Core Java SY BSC CS" in payload_title:
         send_message_to_discord(CORE_JAVA_DC_WEBHOOK, sender, payload_text)
 
     elif "DBMS SY BSC CS" in payload_title:
         send_message_to_discord(DBMS_DC_WEBHOOK, sender, payload_text)
+
+    elif "BSC CS" in payload_title:
+        send_message_to_discord(
+            UNOFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
