21d4679d0f55b0de70cc64d72bae5f1bfbce4389
git-avinash <mail.avinashsah@gmail.com>
2022-01-07 12:23:46 +0530

    fix

--- helpers.py
+++ helpers.py
@@ -20,8 +20,7 @@ def send_message_to_discord(webhook, message_by, message):
 
 def handle_wadc_bridge(form):
     payload_title = form.get("title")
-    parsed_title = [x.strip()
-                    for x in payload_title.split(":") if x.strip() != ""]
+    parsed_title = [x.strip() for x in payload_title.split(":") if x.strip() != ""]
     payload_text = f"```{form.get('text')}```"
     sender = parsed_title[-1] if len(parsed_title) >= 2 else "Unknown"
 
@@ -30,15 +29,7 @@ def handle_wadc_bridge(form):
             return
 
     if "TSDC SYBSc. CS (OFFICIAL)" in payload_title:
-        send_message_to_discord(
-            OFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
-
-    elif "Core Java SY BSC CS" in payload_title:
-        send_message_to_discord(CORE_JAVA_DC_WEBHOOK, sender, payload_text)
-
-    elif "DBMS SY BSC CS" in payload_title:
-        send_message_to_discord(DBMS_DC_WEBHOOK, sender, payload_text)
+        send_message_to_discord(OFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
 
     elif "BSC CS" in payload_title:
-        send_message_to_discord(
-            UNOFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
+        send_message_to_discord(UNOFFICIAL_BSCCS_DC_WEBHOOK, sender, payload_text)
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from helpers import send_message_to_discord
-from defaults import CORE_JAVA_DC_WEBHOOK
+from defaults import *
 
-send_message_to_discord(CORE_JAVA_DC_WEBHOOK,
-                        "Join Lecture Ping", "Ping Test <@&784687737038438440>")
+send_message_to_discord(UNOFFICIAL_BSCCS_DC_WEBHOOK, "Test User", "Ping Test <@&784687737038438440>")
